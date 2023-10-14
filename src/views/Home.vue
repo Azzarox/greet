4dbb7949ae3647dfc3bb71d33532c842e8af02c0
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-14 18:01:17 +0300

    Initialized project with vuetify and Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup>
+// import HelloWorld from "@/components/HelloWorld.vue";
+// import TheNavbar from "@/components/TheNavbar.vue";
+</script>
+
+<template>
+  <h1>ASd</h1>
+</template>
