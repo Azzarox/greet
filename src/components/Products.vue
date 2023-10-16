83037a38bdb1896f3d70cce4a09a47249a3bdf47
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-16 12:25:57 +0300

    Added computed properties for the name and price filter

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import ProductsCard from "./ProductsCard.vue";
 import { VSkeletonLoader } from "vuetify/lib/labs/components.mjs";
 import { baseUrl } from "@/composables/baseUrl.js";
@@ -7,6 +7,7 @@ VSkeletonLoader;
 
 const productsList = ref([]);
 const loadingState = ref(true);
+const filterBy = ref("");
 
 onMounted(async () => {
   try {
@@ -21,15 +22,39 @@ onMounted(async () => {
     console.error("An error occurred:", error);
   }
 });
+
+const filteredProducts = computed(() => {
+  const sortedProductList = [...productsList.value];
+
+  if (filterBy.value === "name") {
+    sortedProductList.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (filterBy.value === "price") {
+    sortedProductList.sort((a, b) => a.prices.price - b.prices.price);
+  }
+
+  return sortedProductList;
+});
 </script>
 
 <template>
   <v-container tag="div">
+    <div class="button-group">
+      <v-btn class="ma-2" variant="outlined" @click.prevent="filterBy = 'name'"
+        >Name</v-btn
+      >
+      <v-btn class="ma-2" variant="outlined" @click.prevent="filterBy = 'price'"
+        >Price</v-btn
+      >
+      <v-btn class="ma-2" variant="outlined" @click.prevent="filterBy = ''"
+        >Reset</v-btn
+      >
+    </div>
+
     <v-row>
       <v-col
         cols="12"
         sm="4"
-        v-for="product in productsList"
+        v-for="product in filteredProducts"
         :key="`product-${product.id}`"
       >
         <ProductsCard :loading="loadingState" :product="product" />
@@ -39,5 +64,10 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-/*  */
+.button-group {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 20px;
+}
 </style>
