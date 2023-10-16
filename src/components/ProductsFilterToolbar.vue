1e347a260e113f3e9c74118a4f4c530de1d87895
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-16 20:01:27 +0300

    Moved logic for toolbar to component

---
+++ src/components/ProductsFilterToolbar.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ProductsFilterByCategories from "./ProductsFilterByCategories.vue";
+import ProductsFilterByNameOrPrice from "./ProductsFilterByNameOrPrice.vue";
+
+const {
+  updateFilterByValueHandler,
+  updateFilterByCategoryHandler,
+  categories,
+} = defineProps({
+  updateFilterByValueHandler: {
+    type: Function,
+    required: true,
+  },
+  updateFilterByCategoryHandler: {
+    type: Function,
+    required: true,
+  },
+  categories: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <v-toolbar :elevation="1" density="comfortable">
+    <v-toolbar-title>Филтри</v-toolbar-title>
+    <v-toolbar-items>
+      <ProductsFilterByNameOrPrice
+        @update-filter-option="updateFilterByValueHandler"
+      />
+      <ProductsFilterByCategories
+        @update-filter-category="updateFilterByCategoryHandler"
+        :categories="categories"
+      />
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<style></style>
