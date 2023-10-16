2c474851157ae0f152d40b34c866c755cb4a9f3c
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-16 11:08:10 +0300

    Using the AddToCartButton component

--- src/components/ProductsCard.vue
+++ src/components/ProductsCard.vue
@@ -1,25 +1,29 @@
 <script setup>
 import { ref } from "vue";
 import { VSkeletonLoader } from "vuetify/lib/labs/components.mjs";
+import AddToCartButton from "@/composables/AddToCartButton.vue";
+
 const props = defineProps({
   product: {
     type: Object,
     required: true,
   },
-  loading:{
+  loading: {
     type: Boolean,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const showDescription = ref(false);
 </script>
 
-
 <template>
   <!-- <v-card class="mx-auto d-flex flex-column" max-width="344" height="100%" > -->
-  <v-card class="mx-auto" max-width="344" height="100%" >
-    <v-skeleton-loader v-if="props.loading === true" type="image,heading, subtitle, ossein, button"></v-skeleton-loader>
+  <v-card class="mx-auto" max-width="344" height="100%">
+    <v-skeleton-loader
+      v-if="props.loading === true"
+      type="image,heading, subtitle, ossein, button"
+    ></v-skeleton-loader>
     <template v-else>
       <v-img
         :src="props.product.images[0].thumbnail"
@@ -38,12 +42,13 @@ const showDescription = ref(false);
             size="small"
             v-for="category in props.product.categories"
             :key="`category-${category.slug}`"
-          >{{ category.name }}</v-chip>
+            >{{ category.name }}</v-chip
+          >
         </v-chip-group>
       </v-card-subtitle>
 
       <v-card-actions>
-        <v-btn color="green-lighten-1" variant="text">Add to cart</v-btn>
+        <AddToCartButton :url="product.add_to_cart.url" :buttonText="product.add_to_cart.text === 'Добавяне в количката' ? product.add_to_cart.text : 'Добави с опции'"/>
 
         <v-spacer></v-spacer>
         <v-btn
