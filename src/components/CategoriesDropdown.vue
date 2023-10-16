ab3e6968bf3832f5c24365bde96fa293400fb995
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-16 18:43:48 +0300

    Added component for the categories filter

---
+++ src/components/CategoriesDropdown.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+});
+
+const pickedCategory = ref("Избери категория");
+const pickCategory = (ev) => {
+  pickedCategory.value = ev.target.textContent;
+};
+</script>
+
+<template>
+  <div class="d-flex justify-space-around">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props">
+          {{ pickedCategory }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(category, index) in props.categories"
+          :key="index"
+          :value="index"
+          @click.prevent="pickCategory"
+        >
+          <v-list-item-title>
+            {{ category }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<style scoped>
+</style>
