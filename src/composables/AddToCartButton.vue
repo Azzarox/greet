6eb95f5cb29f013ea72e7eb01609ab1d06de3677
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-16 11:07:12 +0300

    Added AddToCartButton as a composable and reusable

---
+++ src/composables/AddToCartButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { baseUrl } from "./baseUrl.js";
+
+const { buttonText, url } = defineProps({
+  buttonText: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
+
+console.log(url);
+</script>
+
+<template>
+  <v-btn
+    :href="url.includes('?add-to-cart') ? `${baseUrl}${url}` : url"
+    target="_blank"
+    color="green-lighten-1"
+    variant="text"
+    >{{ buttonText }}</v-btn
+  >
+</template>
+
+<style></style>
