80a9daaf7a11bcae383eaddd7c397a6ad86359bc
azzarox <62171309+Azzarox@users.noreply.github.com>
2023-10-16 18:43:31 +0300

    Added component for the name and price filter

---
+++ src/components/ProductsFilterByNameOrPrice.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, defineEmits } from "vue";
+
+const emits = defineEmits(["update-filter-option"]);
+
+const filteredText = ref("Сортирай по");
+const filteredTextOptions = ["Име", "Цена"];
+
+const selectFilterOption = (selectedOption) => {
+  filteredText.value = selectedOption;
+  emits("update-filter-option", selectedOption);
+};
+</script>
+
+<template>
+  <div class="d-flex justify-space-around">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props">
+          {{ filteredText }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(option, index) in filteredTextOptions"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title @click.prevent="selectFilterOption(option)">{{
+            option
+          }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<style></style>
